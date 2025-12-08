4088108df0ec3d572c728905b38d81b603a6c53f
david <dwu2343@gmail.com>
2025-12-07 18:45:56 -0800

    [FIX] Connect front end and backend for user creation

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,24 @@
-function handleLogin(event) {
+async function handleLogin(event) {
     event.preventDefault();
+
     const email = document.getElementById('email').value;
-    alert('Connexion process for: ' + email);
-    // Connexion process to insert here (backend?)
+    const password = document.getElementById('password').value;
+
+    const response = await fetch("/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        alert("Login successful!");
+
+        // Redirect to homepage
+        window.location.href = "/";
+    } else {
+        alert(data.message || "Invalid credentials");
+    }
 }
+
--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT; 
+const connectDB = require('./config/database');
 
+
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({
+        secret: 'test1',
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+connectDB();
+
+// Routes
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/pages/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/pages/register.html'));
 });
 
 
