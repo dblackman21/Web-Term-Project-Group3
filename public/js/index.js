d85de6dbb8cb8126063bfd500ac296d5aed39793
nk-900 <nekpng@pm.me>
2025-12-11 17:11:11 -0800

    [ADD] Search bar and explore button behavior

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const sideMenuOverlay = document.getElementById("side-menu-overlay");
   const closeMenuBtn = document.getElementById("close-menu-btn");
   const menuIconBtn = document.querySelector(".menu-icon");
+  const cartIconBtn = document.getElementById("cart-icon-btn");
+  const exploreBtn = document.getElementById("explore-collection-btn");
+  const productList = document.getElementById("product-list");
+  const HEADER_OFFESET = 100;
 
   if (loginBtn) {
     loginBtn.addEventListener("click", () => {
@@ -22,16 +26,34 @@ document.addEventListener("DOMContentLoaded", () => {
       sideMenuOverlay.classList.add('open');
     });
   }
+
   if (closeMenuBtn) {
     closeMenuBtn.addEventListener("click", () => {
       sideMenuOverlay.classList.remove('open');
     });
   }
+
   if (sideMenuOverlay) {
     sideMenuOverlay.addEventListener("click", (event) => {
       if (event.target === sideMenuOverlay) {
         sideMenuOverlay.classList.remove('open');
       }
+    });
+  }
+
+  if (cartIconBtn) {
+    cartIconBtn.addEventListener("click", () => {
+      window.location.href = "./pages/cart.html";
+    });
+  }
+
+  if (exploreBtn && productList) {
+    exploreBtn.addEventListener("click", () => {
+      const targetPosition = productList.getBoundingClientRect().top + window.scrollY - HEADER_OFFESET;
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
     });
   }
 });
